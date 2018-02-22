17f3df367f569453e9aaee3c14e0e5cecd063a53
Naval Kishore <naval.kishore@mantralabsglobal.com>
2018-02-22 16:18:19 +0530

    first commit

---
+++ src/payu/PayU.php
@@ -0,0 +1,33 @@
+<?php namespace PayU;
+
+/**
+ * PayU Integration Using Composer
+ */
+class PayU
+{
+    
+    function __construct(argument)
+    {
+        # code...
+    }
+    
+    public function CreditCard()
+    {
+        # code...
+    }
+    
+    public function DebitCard()
+    {
+        # code...
+    }
+    
+    public function NetBanking()
+    {
+        # code...
+    }
+    
+    public function EMI()
+    {
+        # code...
+    }
+}
